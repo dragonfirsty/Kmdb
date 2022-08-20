934d762f68d7933387d6d4565e35445875bc0afd
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-08-19 22:03:39 -0300

    Feat: patch route add and permissions add

--- movies/views.py
+++ movies/views.py
@@ -1,29 +1,24 @@
-from django.contrib.auth import authenticate
 from django.shortcuts import get_object_or_404
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.models import Token
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.views import APIView, Request, Response, status
 
 from .models import Movie
+from .permissions import AdminPermission
 from .serializers import MovieSerializer
 
-# não ta funcionando nenhuma rota ver com o coach amanha!!!!! request.user da uma olhada nisso leo do caralho
+
 class MovieView(APIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AdminPermission]
 
     def get(self, request: Request) -> Response:
         movies = Movie.objects.all()
 
         serializer = MovieSerializer(movies, many=True)
 
-        serializer.is_valid(raise_exception=True)
-
         return Response(serializer.data, status.HTTP_200_OK)
 
     def post(self, request: Request) -> Response:
-        self.permission_classes += [IsAdminUser]
 
         serializer = MovieSerializer(data=request.data)
 
@@ -33,9 +28,10 @@ class MovieView(APIView):
 
         return Response(serializer.data, status.HTTP_201_CREATED)
 
+
 class MovieDetailView(APIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AdminPermission]
 
     def get(self, request: Request, movie_id: int) -> Response:
         movie = get_object_or_404(Movie, id=movie_id)
@@ -44,9 +40,19 @@ class MovieDetailView(APIView):
 
         return Response(serializer.data, status.HTTP_200_OK)
 
-    def delete(self,request:Request, movie_id: int) -> Response:
-        
-        permission_classes += [IsAdminUser]
+    def patch(self,request:Request,movie_id: int)-> Response:
+        movie = get_object_or_404(Movie, id=movie_id)
+
+        serializer = MovieSerializer(movie,data=request.data,partial=True)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+
+    def delete(self, request: Request, movie_id: int) -> Response:
 
         movie = get_object_or_404(Movie, id=movie_id)
 
