0f4a29dec1ed2b5415a72bd415bdf6be5f87630a
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-08-26 21:45:00 -0300

    Feat: ReviewPostSerializer and ReviewSerializer

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,17 +1,26 @@
 from rest_framework import serializers
-
+from users.serializers import UserSerializer
 from .models import Review
 
 
-class ReviewSerializer(serializers.ModelSerializer):
+class ReviewPostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Review
-        fields = ["id","stars","review","spoilers","recomendation","movie","user"]
-        depth = 1
+        fields = ["id","stars","review","spoilers","recomendation","movie_id","user"]
         
-
+        
     def create(self, validated_data: dict) -> Review:
 
         review = Review.objects.create(**validated_data)
 
         return review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Review
+        fields = "__all__"
+        
+        
+
