2afabcefe8a6e66cc26b44252b14c001e5eff4fb
leonardo moya fernandes <95239792+dragonfirsty@users.noreply.github.com>
2022-09-30 16:41:47 -0300

    Fix: secret key fix

--- kmdb/settings.py
+++ kmdb/settings.py
@@ -20,7 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gm1s$ai56y)5-_#b_mo*mt&r#@%cjoc0k@h8b45%chu3(+ty8w"
+import os
+
+import dotenv
+
+dotenv.load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
